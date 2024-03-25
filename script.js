ddd35625b146e07aa31ad67e448ee499fdea0f4b
agustinushenan01 <agustinushenan01@gmail.com>
2024-03-26 04:59:20 +0800

    slide belum jadi

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 const menuIcon = document.getElementById("burgermenu");
 const menuList = document.getElementById("navbar");
 
-menuIcon.addEventListener("click", () =>{
+menuIcon.addEventListener("click", () => {
     menuList.classList.toggle("hidden");
 });
+let currentIndex = 0; // Deklarasi currentIndex di luar fungsi
 
-const images = document.querySelectorAll('.content-image img');
-        let currentIndex = 0;
-        const containerWidth = document.querySelector('.content-image').offsetWidth;
+function changeImage() {
+    const images = document.querySelectorAll('.content1-image img');
+    const containerWidth = document.querySelector('.content1-image').offsetWidth;
 
-        function changeImage() {
-            // Geser ke gambar berikutnya dengan efek animasi dari kiri ke kanan
-            currentIndex = (currentIndex + 1) % images.length;
-            images.forEach((image, index) => {
-                const newPosition = (index - currentIndex) * containerWidth;
-                image.style.transition = 'transform 1s ease-in-out';
-                // Perubahan arah translasi dari kiri ke kanan (positive)
-                image.style.transform = `translateX(${newPosition}px)`;
-            });
-        }
+    // Geser ke gambar berikutnya dengan efek animasi dari kiri ke kanan
+    currentIndex = (currentIndex + 1) % images.length;
 
-        setInterval(changeImage, 3000);
+    // Atur posisi ulang untuk gambar pertama setelah gambar terakhir
+    if (currentIndex === 0) {
+        images.forEach(image => {
+            image.style.transition = ''; // Hilangkan transisi untuk menghindari animasi saat mengatur ulang posisi
+            image.style.transform = `translateX(0)`;
+        });
+    }
+
+    // Geser posisi awal untuk gambar berikutnya
+    let initialPosition = 0;
+    images.forEach((image, index) => {
+        // Geser gambar ke kiri
+        image.style.transition = 'transform 1s ease-in-out';
+        image.style.transform = `translateX(-${initialPosition}px)`;
+        // Geser posisi awal untuk gambar berikutnya
+        initialPosition += containerWidth;
+    });
+}
+
+setInterval(changeImage, 3000);
+
